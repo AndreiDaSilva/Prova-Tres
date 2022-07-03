6bf048205b15c44c18ad8fbb9086f522eb753687
Andrei Robson da Silva <andreirobson@furb.br>
2022-07-03 18:53:33 -0300

    method GET

--- ProvaTres/js/restApi.js
+++ ProvaTres/js/restApi.js
@@ -1,15 +1,25 @@
-// Get API RESTFul
+const url = "https://bu.furb.br/mcardoso/progWeb/apiRestAval.php/cadastro/";
 
-function consultarCodigo(id) {
-    fetch("https://bu.furb.br/mcardoso/progWeb/apiRestAval.php/cadastro/" + id, {method:"get"})
-    .then((response) => response.json())
-    .then((data) => {
-        document.getElementById('codigo').value = data.id;
-        document.getElementById('nome').value = data.nome;
-        document.getElementById('departamento').value = data.departamento;
-        document.getElementById('endereco').value = data.endereco;
-        document.getElementById('email').value = data.email;
-    })
+
+//GET Dado
+async function consultarCodigo(id) {
+    const response = await fetch(url + id, {method: "GET"});
+    const data = await response.json();
+    getNome(data);
+    getEmail(data);
+
 }
+
+function getNome(data){
+    const nome = data.nome;
+    console.log(nome);
+}
+
+function getEmail(data) {
+    const email = data.email;
+    console.log(email);
+}
+
+
 
 
